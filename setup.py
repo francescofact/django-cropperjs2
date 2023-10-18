8d1190e415f834e4ac7367b9ebafdcc9d3aa5174
ffattori <ffattori@tesla.com>
2023-10-18 11:07:34 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-cropperjs',
+    name='django-cropperjs2',
     version='0.0.6',
     packages=find_namespace_packages(),
     include_package_data=True,
@@ -16,8 +16,8 @@ setup(
     description='Adds CropperJS\'s functionality to image fields in Django/Django CMS admin.',
     long_description=README,
     url='https://bitbucket.org/rcostanza/django-cropperjs/',
-    author='Ronaldo Alves',
-    author_email='setembro@gmail.com',
+    author='Francesco Fattori',
+    author_email='ffattori@me.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
